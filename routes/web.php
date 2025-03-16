446b81580afefb0eaa94edb115d8185a8a61e44a
Manuel Kyd Thomas Nagpala <kydnagpala.dev@gmail.com>
2025-03-16 14:31:19 +0800

    feat: group the user route

--- routes/web.php
+++ routes/web.php
@@ -4,11 +4,15 @@ use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 
-Route::get('/user', [UserController::class, 'index']);
-Route::post('/user', [UserController::class, 'store'])->name('user.store');
-Route::get('/user/create', [UserController::class, 'create']);
-Route::get('/user/{user}', [UserController::class, 'show'])->name('user.show');
-Route::get('/user/{user}/edit', [UserController::class, 'edit'])->name('user.edit');
-Route::put('/user/{user}/update', [UserController::class, 'update'])->name('user.update');
-Route::put('/user/{user}/update', [UserController::class, 'update'])->name('user.update');
-Route::delete('/user/{user}/destroy', [UserController::class, 'destroy'])->name('user.destroy');
+Route::controller(UserController::class)->group(function () {
+  Route::get('/user', 'index');
+  Route::post('/user', 'store')->name('user.store');
+  Route::get('/user/create', 'create');
+  Route::get('/user/{user}', 'show')->name('user.show');
+  Route::get('/user/{user}/edit', 'edit')->name('user.edit');
+  Route::put('/user/{user}/update', 'update')->name('user.update');
+  Route::delete('/user/{user}/destroy', 'destroy')->name('user.destroy');
+});
+
+
+
